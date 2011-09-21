172174913bda7deab2c02bad9182e80e65015ed0
Peter Tseng <patseng@stanford.edu>
2011-09-21 12:44:29 -0700

    Added Multiple Constructor hack.  Nodes can be initialized from a parsed json.  Processing written in pure javascript code is in place

--- matseng/visuall_v1.js
+++ matseng/visuall_v1.js
@@ -1,89 +1,78 @@
-function Node(title, x, y) {
-	this.title = title;
-	this.x = x;
-	this.y = y;
+//hack for multiple constructors
+function Node(titleOrJSON,x,y) {
+    if (arguments.length == 3) {
+        this.title = titleOrJSON;
+        this.x = x;
+        this.y = y;
+    } else if (arguments.length == 1) {
+        this.title = titleOrJSON.title;
+        this.x = titleOrJSON.x;
+        this.y = titleOrJSON.y;
+    } else {
+        throw new Error("Invalid arguments");
+    }
 }
 
-var n1 = new Node("Mike", 1, 1);
-var myNodeStr = JSON.stringify(n1);
-//alert(myNodeStr);
-//alert(n1.title);
-myjson = JSON.parse(myNodeStr);
-alert(myjson);	
 
+function loadNodelistFromServer(){
+    //this is to set up what the json might look like on the server
+    var nodelistFromServer = [];
+    var n1 = new Node("Peter",750,750);
+    var n2 = new Node("Mike",750,250);
+    var n3 = new Node("Jossy",750,500);
+    var n4 = new Node("GMA",500,500);
+    nodelistFromServer.push(n1);
+    nodelistFromServer.push(n2);
+    nodelistFromServer.push(n3);
+    nodelistFromServer.push(n4);
 
+    //we would get this variable from the server
+    var unparsedNodelistJSON = JSON.stringify(nodelistFromServer);
 
-// 
-// function Node() {
-// 	this.id=-1;
-// 	this.title = "blank title";
-// 	this.x = -1;
-// 	this.y = -1;
-// 	this.edges=[ ]; //edge Ids
-// 	
-// 	Node.prototype.init = function(_id, _title, _x, _y, _edges){
-// 		this.id = _id;
-// 		this.title = _title;
-// 		this.x = _x;
-// 		this.y = _y;
-// 		this.edges = _edges;
-// 	}
-// 	/*
-// 	if(typeof this.setTitle !== "function") {
-// 		Node.prototype.setTitle = function( newTitle ) {
-// 		this.title = newTitle;
-// 		}
-// 	}
-// 	if(typeof this.getTitle !== "function") {
-// 		Node.prototype.getTitle = function() {
-// 		return this.title;
-// 		}
-// 	}
-// 	*/
-// }
-// 
-// function Edge() {
-// 	this.id = -1;
-// 	this.title = "blank edge title";
-// 	this.nodeId_1 = "blank node1";
-// 	this.nodeId_2 = "blank node2";
-// 	
-// 	Edge.prototype.init = function(_id, _title, _nodeId_1, _nodeId_2){
-// 		this.id = _id;
-// 		this.title = _title;
-// 		this.nodeId_1 = _nodeId_1; //starting node
-// 		this.nodeId_2 = _nodeId_2; //ending node
-// 	}
-// }
-// 
-// 
-// 
-// /* MAIN */
-// var helloWorld = new Node();
-// helloWorld.init(0, "Hello World", 64, 128, [1] );
-// 
-// var grandma = new Node();
-// grandma.init(1, "Grandma", 128, 128, [0, 1, 2] );
-// 
-// var michael = new Node();
-// michael.init(2, "Michael", 96, 192, [1] );
-// 
-// var peter = new Node();
-// peter.init(3, "Peter", 160, 192, [2] );
-// 
-// var edgeTop = new Edge();
-// edgeTop.init(0, "Top edge", 0, 1 );
-// 
-// var edgeLeft = new Edge();
-// edgeLeft.init(1,"Left edge", 1, 2);
-// 
-// var edgeRight = new Edge();
-// edgeRight.init(2, "Right edge", 1, 3)
-// 
-// 
-// alert(grandma.edges);
-// alert(edgeLeft.nodeId_2);
+    var parsedNodelistJSON = jQuery.parseJSON(unparsedNodelistJSON);
+    return createNodesFromParsedJSON(parsedNodelistJSON);
+}
 
-//myNode.setTitle("Testing my first node");
-//alert(myNode.getTitle());
-//alert("Hello vvorld!");
+function createNodesFromParsedJSON(parsedJSONNodelist) {
+    var nodelist = [];
+    for (var i = 0; i < parsedJSONNodelist.length; ++i) {
+        var node = new Node(parsedJSONNodelist[i]);
+        nodelist.push(node);
+    }
+    return nodelist;
+}
+
+function visualizeVisuall(processing) {
+   var nodelist = [];
+
+    processing.setup = function() {
+        processing.size(1000,1000);
+        processing.background(125);
+        processing.fill(255);
+        processing.strokeWeight(2);
+        processing.noLoop();
+
+        nodelist = loadNodelistFromServer();
+    }
+
+    processing.draw = function() {
+        processing.background(100);
+        for (var i = 0; i < nodelist.length; i++) {
+            draw(nodelist[i]);
+        }
+    }
+
+    function draw(node) {
+        processing.stroke(255);
+        processing.fill(0,121,184);
+        processing.ellipseMode(processing.CENTER);
+        processing.ellipse(node.x, node.y,100,100);
+        processing.fill(255);
+        processing.textAlign(processing.CENTER);
+        processing.text(node.title, node.x, node.y);
+    }
+}
+
+//This sets up the canvas to allow processing to do all of our drawing
+var canvas = document.getElementById("canvas1");
+var processing = new Processing(canvas, visualizeVisuall);
